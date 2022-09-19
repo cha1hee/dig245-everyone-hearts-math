5f7404c1337c7eb6bcc2f7f7da19ab2764ffb77b
Pauline Cha <pacha@davidson.edu>
2022-09-19 15:46:24 -0400

    classwork updates with jquery and bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-
 /* javascript */
+
+$("h1").hide();
+$(".clickme").click(function(eventObject){
+  console.log("hello!", eventObject);
+});
+
+let username = "";
+
+$("form").submit(function(e){
+  e.preventDefault();
+  username = $(".username").val();
+  console.log(username);
+});
